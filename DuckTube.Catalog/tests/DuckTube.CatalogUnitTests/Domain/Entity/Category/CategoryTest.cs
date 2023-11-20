b54de0fa9985f5b697eaba655d8bec582483c17d
Thalita Mariano <8.litah.8@gmail.com>
2023-11-20 00:49:52 -0300

    initialize project

---
+++ DuckTube.Catalog/tests/DuckTube.CatalogUnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace DuckTube.CatalogUnitTests.Domain.Entity.Category;
+
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instatiate))]
+    [Trait("Domain", "Category - Agregates")]
+    public void Instatiate()
+    {
+        //Arange
+        var validData = new
+        {
+            Name = "Category Name",
+            Description = "Category Description",
+        };
+
+        //Act
+        var category = new Category(validData.Name, validData.Description);
+        
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+     }
+}
+
